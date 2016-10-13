ea3219f91edcc840fa0d112afe26b6d709123222
Xu <email.xhuang@gmail.com>
2016-10-13 00:22:22 -0400

    commit 2

---
+++ navscript.js
@@ -0,0 +1,9 @@
+// Navbar open:
+function openNav() {
+    document.getElementById("navbar").style.height = "100%";
+}
+
+// Navbar close:
+function closeNav() {
+    document.getElementById("navbar").style.height = "0%";
+}
