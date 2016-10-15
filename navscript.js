219d46518642376c572ffe3661525fe3ec6cd0af
Xu <email.xhuang@gmail.com>
2016-10-15 01:14:17 -0400

    Finished ABOUT section & minor fixes

--- navscript.js
+++ navscript.js
@@ -1,9 +1,9 @@
 // Navbar open:
 function openNav() {
-    document.getElementById("navbar").style.height = "100%";
+    document.getElementById("navbar").style.width = "15%";
 }
 
 // Navbar close:
 function closeNav() {
-    document.getElementById("navbar").style.height = "0%";
+    document.getElementById("navbar").style.width = "0%";
 }
